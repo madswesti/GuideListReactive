8874226eae24dbe414be61ba241d1cad77054f64
Mads Westi <post@madswesti.dk>
2019-04-28 21:24:34 +0200

    Configure app

--- Source/App/AppDelegate.swift
+++ Source/App/AppDelegate.swift
@@ -7,13 +7,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		
-		let rootViewController = UIViewController()
-		rootViewController.view.backgroundColor = .white
+		let rootViewController = GuidesTableViewController(viewModel: GuideListViewModel(service: Service()))
 		
 		self.window = UIWindow()
-		self.window?.rootViewController = rootViewController
+		self.window?.rootViewController = UINavigationController(rootViewController: rootViewController)
 		self.window?.makeKeyAndVisible()
 		
 		return true
+	}
+}
+
+
+private extension Service {
+	// Would be handle by some DI framework or similar, in a real application.
+	convenience init() {
+		let baseURL = URL(string: "https://private-c60ade-guidebook1.apiary-mock.com")!
+		let networkClient = NetworkClient(baseURL: baseURL)
+		self.init(networkClient: networkClient)
 	}
 }
